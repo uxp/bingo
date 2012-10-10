c79acec442d124f890aa61f527e9f4b24a5240f1
Howard P. Logsdon <howard@hplogsdon.com>
2012-10-10 10:21:37 -0600

    load values into new variable, then initialize seperate vars for the freespace and values array, which solves the duplication problem

--- lib/bingo/card.rb
+++ lib/bingo/card.rb
@@ -5,7 +5,9 @@ module Bingo
     attr_reader :grid
 
     def initialize
-      values = load_values
+      wordlist  = load_values
+      values    = wordlist['values']
+      freespace = wordlist['freespace']
 
       @grid = Array.new
 
@@ -14,9 +16,9 @@ module Bingo
         5.times do |row|
           # on the center square, set a predefined value
           if row == 2 and column == 2
-            @grid[column][row] = "#{values['freespace']['value']}#{values['freespace']['html']}"
+            @grid[column][row] = "#{freespace['value']}#{freespace['html']}"
           else
-            @grid[column][row] = values['values'].sample
+            @grid[column][row] = values.sample
             values.delete @grid[column][row]
           end
         end
